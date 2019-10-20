e33d08a5714334a089674bcf026ed8d4af6879d9
Andrew Brenev <andrei.brenev@mail.ru>
2019-10-20 21:43:29 +0700

    add set of  sorted images

--- Pyramid/mainwindow.cpp
+++ Pyramid/mainwindow.cpp
@@ -1,19 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent)
+	: QMainWindow(parent)
+	, ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
 
-    currentPixmap= QPixmap("C:/gitProjects/freeCode/Pyramide/Pyramid/img/mount.jpg" );
-    //iteam = new QGraphicsPixmapItem(currentPixmap);
-    iteam = nullptr;
-    scene = new QGraphicsScene(this);
-    ui->graphicsView->setScene(scene);
-   // scene->addItem(iteam);
-   // updateLables();
+	iteam = nullptr;
+	scene = new QGraphicsScene(this);
+	ui->graphicsView->setScene(scene);
 
 }
 
@@ -25,63 +21,88 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionAdd_new_triggered()
 {
-    QFileDialog dialog(this);
-    dialog.setNameFilter(tr("Images (*.png *.jpg)"));
-    dialog.setViewMode(QFileDialog::Detail);
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"),"/home",tr("Images (*.png *.jpg)"));
+	QFileDialog dialog(this);
+	dialog.setNameFilter(tr("Images (*.png *.jpg)"));
+	dialog.setViewMode(QFileDialog::Detail);
+	QString filePath = QFileDialog::getOpenFileName(this, tr("Open File"), "/home", tr("Images (*.png *.jpg)"));
 
-    currentPixmap = QPixmap::fromImage(QImage (fileName) );
-    showCurrentPixmar(currentPixmap);
+	currentObject = QPixmap(filePath);
+	filePath = filePath.mid(filePath.lastIndexOf("/") + 1);
+	Image currentImage(currentObject, filePath);
+	imagesList.insert(currentImage);
+	setLoadedFiles(filePath);
+	showCurrentPixmap(buildPyramidLevel(currentObject, ui->spinBox->value(), ui->doubleSpinBox->value()));
 
 }
 
-void MainWindow::showCurrentPixmar(const QPixmap &pixmapToShow)
+void MainWindow::setLoadedFiles(const QString& activeImage)
 {
-    delete iteam;
-    scene->clear();
-    iteam = new QGraphicsPixmapItem (pixmapToShow);
-    scene->setSceneRect(pixmapToShow.rect());
-    scene->addItem(iteam);
-    updateLables();
+	ui->comboBox->clear();
+
+	int distance = 0;
+	for (auto img : imagesList)
+	{
+		ui->comboBox->addItem(img.fileName);
+		distance++;
+	}
+	ui->comboBox->setCurrentText(activeImage);
 }
 
-void MainWindow::updateLables()
+void MainWindow::showCurrentPixmap(const QPixmap& pixmapToShow)
 {
-  //  ui->widthValuelabel->setNum(currentImage->width());
-  //  ui->heightValuelabel->setNum(currentImage->height());
+	delete iteam;
+	scene->clear();
+	iteam = new QGraphicsPixmapItem(pixmapToShow);
+	scene->setSceneRect(pixmapToShow.rect());
+	scene->addItem(iteam);
+	ui->sizeLable->setText(QString::number(currentObject.width()) + "x" + QString::number(currentObject.height()));
 }
 
-QPixmap MainWindow:: buildPyramidLevel(const QPixmap& basePixmap,int level,double crop)
+
+QPixmap MainWindow::buildPyramidLevel(const QPixmap& basePixmap, int level, double crop)
 {
-    int scale = pow(crop,level);
-    QPixmap resPixmap = basePixmap.scaled(basePixmap.width()/ scale, basePixmap.height() / scale,Qt::KeepAspectRatio);
-    return resPixmap.scaled(basePixmap.width(),basePixmap.height(),Qt::KeepAspectRatio);
+	if (&basePixmap != nullptr && !basePixmap.isNull())
+	{
+		int scale = pow(crop, level);
+		QPixmap resPixmap = basePixmap.scaled(basePixmap.width() / scale, basePixmap.height() / scale, Qt::KeepAspectRatio);
+		return resPixmap.scaled(basePixmap.width(), basePixmap.height(), Qt::KeepAspectRatio);
+	}
 }
 
 void MainWindow::on_spinBox_valueChanged(int arg)
 {
-   if( ui->doubleSpinBox->value()> 0 && arg > 0)
-    showCurrentPixmar(buildPyramidLevel(currentPixmap,arg,ui->doubleSpinBox->value()));
+	if (ui->doubleSpinBox->value() > 0 && arg > 0)
+		showCurrentPixmap(buildPyramidLevel(currentObject, arg, ui->doubleSpinBox->value()));
 }
 
-void MainWindow::on_spinBox_valueChanged(const QString &arg)
+void MainWindow::on_spinBox_valueChanged(const QString& arg)
 {
-    if( ui->doubleSpinBox->value()> 0 && arg.toInt() > 0)
-     showCurrentPixmar(buildPyramidLevel(currentPixmap,arg.toInt(),ui->doubleSpinBox->value()));
+	if (ui->doubleSpinBox->value() > 0 && arg.toInt() > 0)
+		showCurrentPixmap(buildPyramidLevel(currentObject, arg.toInt(), ui->doubleSpinBox->value()));
 }
 
 void MainWindow::on_doubleSpinBox_valueChanged(double arg)
 {
-    if(ui->spinBox->value() > 0 && arg>0)
-        showCurrentPixmar(buildPyramidLevel(currentPixmap,ui->spinBox->value(),arg));
+	if (ui->spinBox->value() > 0 && arg > 0)
+		showCurrentPixmap(buildPyramidLevel(currentObject, ui->spinBox->value(), arg));
 
 }
 
-void MainWindow::on_doubleSpinBox_valueChanged(const QString &arg)
+void MainWindow::on_doubleSpinBox_valueChanged(const QString& arg)
 {
-    if(ui->spinBox->value() > 0 && arg.toDouble()>0 )
-        showCurrentPixmar(buildPyramidLevel(currentPixmap,ui->spinBox->value(),arg.toDouble()));
+	if (ui->spinBox->value() > 0 && arg.toDouble() > 0)
+		showCurrentPixmap(buildPyramidLevel(currentObject, ui->spinBox->value(), arg.toDouble()));
 
 }
 
 
+void MainWindow::on_comboBox_currentIndexChanged(int index)
+{
+	if (index >= 0)
+	{
+		auto it = imagesList.begin();
+		std::advance(it, index);
+		currentObject = it->pixmap;
+		showCurrentPixmap(buildPyramidLevel(currentObject, ui->spinBox->value(), ui->doubleSpinBox->value()));
+	}
+}
--- Pyramid/mainwindow.h
+++ Pyramid/mainwindow.h
@@ -8,39 +8,61 @@
 #include <QtGui>
 #include <QFileDialog>
 
+#include <set>
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class MainWindow;
-class QImage;
-class QPixmap;
+	class MainWindow;
+	class QImage;
+	class QPixmap;
 }
 QT_END_NAMESPACE
 
+class Image
+{
+public:
+	QPixmap pixmap;
+	QString fileName;
+
+	Image() {}
+	~Image() {}
+	Image(const QPixmap& _pixmap, const QString& _fileName) :pixmap(_pixmap), fileName(_fileName) {}
+	bool operator < (const Image& compElem) const
+	{
+		double firstDiagonal = sqrt(pixmap.width() * pixmap.width() + pixmap.height() * pixmap.height());
+		double secondDiagonal = sqrt(compElem.pixmap.width() * compElem.pixmap.width() + compElem.pixmap.height() * compElem.pixmap.height());
+		return firstDiagonal < secondDiagonal;
+	}
+};
+
+
+using QPixmapContainer = std::set<Image>;
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+	MainWindow(QWidget* parent = nullptr);
+	~MainWindow();
 
 private slots:
-    void on_actionAdd_new_triggered();
-    void on_doubleSpinBox_valueChanged(double arg1);
-    void on_doubleSpinBox_valueChanged(const QString &arg1);
-
-    void on_spinBox_valueChanged(const QString &arg1);
+	void on_actionAdd_new_triggered();
+	void on_doubleSpinBox_valueChanged(double arg1);
+	void on_doubleSpinBox_valueChanged(const QString& arg1);
+	void on_spinBox_valueChanged(const QString& arg1);
+	void on_spinBox_valueChanged(int arg1);
+	void on_comboBox_currentIndexChanged(int index);
 
 private:
-    QPixmap buildPyramidLevel(const QPixmap& basePixmap,int level,double crop);
-    void updateLables();
-    void showCurrentPixmar(const QPixmap &pixmapToShow);
-    void on_spinBox_valueChanged(int arg1);
+	QPixmap buildPyramidLevel(const QPixmap& basePixmap, int level, double crop);
+	void showCurrentPixmap(const QPixmap& pixmapToShow);
+	void setLoadedFiles(const QString& activeImage);
 
-    Ui::MainWindow *ui;
-    QPixmap currentPixmap;
-    QGraphicsItem *iteam;
-    QGraphicsScene *scene;
+	QPixmapContainer imagesList;
+	QPixmap currentObject;
+	Ui::MainWindow* ui;
+	QGraphicsItem* iteam;
+	QGraphicsScene* scene;
 };
 #endif // MAINWINDOW_H
